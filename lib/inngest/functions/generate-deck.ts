e05c8294f4e93702a9dbeef2982c66cfc7802e83
yogya21 <yogyachhatwani@gmail.com>
2026-09-11 21:56:33 +0530

    inngest setup done in this commit

---
+++ lib/inngest/functions/generate-deck.ts
@@ -0,0 +1,12 @@
+import { inngest } from "../client";
+
+export const generateDeck = inngest.createFunction(
+    {
+        id: "generate-deck",
+        triggers: []
+    },
+
+    async ({ event, step }) => {
+
+    }
+);
